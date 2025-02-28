3024158b00a4f511546b05fb14e69863ec64da12
Yu Liming <lmyu@smail.nju.edu.cn>
2025-02-28 19:19:53 +0800

    Add files via upload

---
+++ id_get_code.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+# 读取数据
+data_file = "info_code.csv"  # CSV文件格式："姓名,身份证号,密码"
+df = pd.read_csv(data_file)
+
+st.title("获取密码")
+
+name = st.text_input("姓名")
+id_number = st.text_input("身份证号")
+
+if st.button("查询"):
+    user = df[(df["姓名"] == name) & (df["身份证号"] == id_number)]
+    if not user.empty:
+        password = user["新班级"].values[0]
+        st.success(f"新班级：{password}")
+    else:
+        st.error("未找到匹配信息，请检查输入是否正确。")
